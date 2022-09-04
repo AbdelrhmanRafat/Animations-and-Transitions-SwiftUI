3cde160dffca4527e78c9873df014ed544791be8
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2022-09-04 14:59:39 +0200

    Initial Commit

---
+++ Animations and Transitions SwiftUI/Animations_and_Transitions_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animations_and_Transitions_SwiftUIApp.swift
+//  Animations and Transitions SwiftUI
+//
+//  Created by Macbook on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Animations_and_Transitions_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
