e4cb237db32ca91a5b780c0e42fc7cbdd956a17f
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-15 19:15:51 +0200

    ‘Implicit_Animation’

--- Animations and Transitions SwiftUI/ContentView.swift
+++ Animations and Transitions SwiftUI/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var circleColorChanged = false
+    @State private var heartColorChanged = false
+    @State private var heartSizeChanged = false
+    //You Define Two State of view and swiftUI Will figure out the rest.
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Circle()
+                .frame(width: 200, height: 200, alignment: .center)
+                .foregroundColor(circleColorChanged ? Color(.systemGray) : .red)
+                .animation(.default)// Built in Animation.
+            Image(systemName: "heart.fill")
+                .foregroundColor(heartColorChanged ? .red : .white)
+                .font(.system(size: 100))
+                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
+                .animation(.default)
+        }
+        .onTapGesture {
+            self.circleColorChanged.toggle()
+            self.heartColorChanged.toggle()
+            self.heartSizeChanged.toggle()
+        }
     }
 }
 
