1fed65ba9cc0899c20b1b9b8bdb59ee6e36f4f06
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-19 18:55:54 +0200

    Loading Indicator Using Rounded Rectangle.

--- Animations and Transitions SwiftUI/ContentView.swift
+++ Animations and Transitions SwiftUI/ContentView.swift
@@ -9,20 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isLoading = false
-    //Loading Indicator Using Rotation Effect.
+    //Loading Indicator Using Rounded Rectangular.
     var body: some View {
-        Circle()
-            .trim(from: 0.0, to: 0.7)
-            .stroke(Color.green,lineWidth: 5)
-            .frame(width: 100, height: 100, alignment: .center)
-            .rotationEffect(Angle(degrees: isLoading ? 360.0 : 0.0))// Two States that the Loading status will control.
-            //Repeat Forever Repeat Animation again and again.
-            .animation(Animation.default.repeatForever(autoreverses: false))
-            .onAppear(){
-                self.isLoading = true
-            }
+        ZStack {
+            Text("Loading")
+                .font(.system(.body, design: .rounded))
+                .fontWeight(.bold)
+                .offset(x: 0, y: -25)
+              RoundedRectangle(cornerRadius: 3)
+                .stroke(Color(.systemGray5),lineWidth: 3)
+                .frame(width: 250, height: 3, alignment: .center)
+            RoundedRectangle(cornerRadius: 3)
+                .stroke(Color.green,lineWidth: 3)
+                .frame(width: 30, height: 3, alignment: .center)
+                .offset(x: isLoading ? 110 : -100, y: 0)
+                .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+        }
+        .onAppear(){
+            self.isLoading = true
+        }
+        
+        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
