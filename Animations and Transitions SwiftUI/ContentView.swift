00177e38ec7c5a59d57522b3502337fe19e265c4
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-19 19:36:27 +0200

    Progress Indicator.

--- Animations and Transitions SwiftUI/ContentView.swift
+++ Animations and Transitions SwiftUI/ContentView.swift
@@ -8,29 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress : CGFloat = 0.0
     @State private var isLoading = false
-    //Loading Indicator Using Rounded Rectangular.
+    //Progress Indicator
     var body: some View {
         ZStack {
-            Text("Loading")
-                .font(.system(.body, design: .rounded))
+            Text("\(Int(progress * 100))%")
+                .font(.system(.title, design: .rounded))
                 .fontWeight(.bold)
-                .offset(x: 0, y: -25)
-              RoundedRectangle(cornerRadius: 3)
-                .stroke(Color(.systemGray5),lineWidth: 3)
-                .frame(width: 250, height: 3, alignment: .center)
-            RoundedRectangle(cornerRadius: 3)
-                .stroke(Color.green,lineWidth: 3)
-                .frame(width: 30, height: 3, alignment: .center)
-                .offset(x: isLoading ? 110 : -100, y: 0)
-                .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+            Circle()
+                .stroke(Color(.systemGray5),lineWidth: 10)
+                .frame(width: 150, height: 150, alignment: .center)
+            Circle()
+                .trim(from: 0.0, to: progress)
+                .stroke(Color.green,lineWidth: 10)
+                .frame(width: 150, height: 150, alignment: .center)
+                .rotationEffect(Angle(degrees: -90))
         }
         .onAppear(){
-            self.isLoading = true
-        }
-        
+            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
+                self.progress += 0.05
+                if self.progress >= 1.0 {
+                    timer.invalidate() // Stops the timer.
+                }
+            }
         }
     }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
