9c07c7451444aa5aeb54c23343a14703a7baf9c4
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-19 20:53:33 +0200

    Default Transition

--- Animations and Transitions SwiftUI/ContentView.swift
+++ Animations and Transitions SwiftUI/ContentView.swift
@@ -8,37 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var recordBegin = false
-    @State private var recording = false
-    //Transforming Rectangle into Circle.
+    //Understanding transitions
+    @State private var show = false
     var body: some View {
-        ZStack {
-            // Rounded Rectangle Transfer to circle..When the corner redius be equal to half of Frame Width..
-            RoundedRectangle(cornerRadius: recordBegin ? 30 : 5) // Corner Redius is 5 at the begin for Rectangle
-                .frame(width: recordBegin ? 60 : 250, height: 60, alignment: .center) // Rectangle redius is 250 at the begin
-                .foregroundColor(recordBegin ? .red : .green) // Rectangle color is green at the begin.
-            RoundedRectangle(cornerRadius: recordBegin ? 35 : 10)
-                .trim(from: 0, to: recordBegin ? 0 : 1)
-                .stroke(lineWidth: 5)
-                    .frame(width: recordBegin ? 70 : 260, height: 70, alignment: .center)
-            .foregroundColor(.green)
-            .overlay(
-                Image(systemName: "mic.fill") // Over Lay Mic Image on Rectangle
-                    .font(.system(.title))
+        VStack {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300, alignment: .center)
+                .foregroundColor(.green)
+                .overlay(
+                Text("Show Details")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .bold()
                     .foregroundColor(.white)
-                    .scaleEffect(recording ? 0.7 : 1) // Mic Scale Changed between 0.7 and 1 with spring Animation..
                 )
-        }
+            if show {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300, alignment: .center)
+                .foregroundColor(.purple)
+                .overlay(
+                Text("Well Here's the details")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                )
+           }
+       }
         .onTapGesture {
             withAnimation(Animation.spring()){
-                self.recordBegin.toggle()
-            }
-            withAnimation(Animation.spring().repeatForever().delay(0.5)){ // Delay Used to show spring animation for mic after transfer from rectangle to circle done
-                self.recording.toggle()
+                self.show.toggle()
             }
         }
-        
-    }
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
